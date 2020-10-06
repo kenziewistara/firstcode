7d85c05bfe3005e8fbf8f84745575ff3d4dc7d28
Kenzie Wistara <69298824+kenziewistara@users.noreply.github.com>
2020-10-06 09:24:41 +0700

    Update 06102020

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -17,14 +17,14 @@ class Admin extends CI_Controller
 
 		if ($this->form_validation->run() == FALSE) {
 			$this->load->view('templates/header');
-			$this->load->view('pulsa/index');
+			$this->load->view('admin/pulsa/index');
 			$this->load->view('templates/footer');
 		} else {
 			$data['telepon'] = $this->input->post('telepon');
 			$data['provider'] = $this->input->post('provider');
 			$data['nominal'] = $this->input->post('nominal');
 			$this->load->view('templates/header', $data);
-			$this->load->view('pulsa/index', $data);
+			$this->load->view('admin/pulsa/index', $data);
 			$this->load->view('templates/footer', $data);
 		}
 	}
